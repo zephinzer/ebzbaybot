024edc714674552c7d4674b75f25ac8e0859b01e
zephinzer <zephinzer@users.noreply.github.com>
2022-03-28 16:58:03 +0800

    added versioning stuff

--- cmd/ebzbot/command.go
+++ cmd/ebzbot/command.go
@@ -4,12 +4,14 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/zephinzer/ebzbaybot/cmd/ebzbot/get"
 	"github.com/zephinzer/ebzbaybot/cmd/ebzbot/start"
+	"github.com/zephinzer/ebzbaybot/internal/constants"
 )
 
 func GetCommand() *cobra.Command {
 	command := cobra.Command{
-		Use:  "ebzbot",
-		Long: "This is an unofficial bot for monitoring NFTs on Ebisus's Bay (https://app.ebisusbay.com)",
+		Use:     "ebzbot",
+		Long:    "This is an unofficial bot for monitoring NFTs on Ebisus's Bay (https://app.ebisusbay.com)",
+		Version: constants.Version,
 	}
 	command.AddCommand(get.GetCommand())
 	command.AddCommand(start.GetCommand())
