ebcb5dcaa75cc3a9785253bc8fb15143e2a51467
zephinzer <zephinzer@users.noreply.github.com>
2022-03-30 17:55:21 +0800

    added donation currencies

--- internal/telegram/handlers/help.go
+++ internal/telegram/handlers/help.go
@@ -11,7 +11,7 @@ func HandleHelp(opts Opts) error {
 	msg := tgbotapi.NewMessage(opts.Update.Message.Chat.ID, fmt.Sprintf(
 		"🎣 I'm @EbzBayBot and I help you with NFT adventures at the [Ebisus Bay NFT Marketplace](https://app.ebisusbay.com) by notifying you about changes to collections you're eyeing.\n\n"+
 			"⚙️ Check out my [code on GitHub](https://github.com/zephinzer/ebzbaybot)\n\n"+
-			"💡 Donations address for server costs + eternal gratitude: `%s`\n\n"+
+			"💡 Donations address for server costs + eternal gratitude ($CRO/$ETH): `%s`\n\n"+
 			"*🪓 To control me, use*:\n"+
 			"`/get` gets information about a collection\n"+
 			"`/watch` starts watching a collection\n"+
