d22deec0659e0a61583f94aba235fc8f92098018
zephinzer <zephinzer@users.noreply.github.com>
2022-03-30 12:34:46 +0800

    removed fk because fk fks

--- data/migrations/202203292358_create_floor_price_diffs.up.sql
+++ data/migrations/202203292358_create_floor_price_diffs.up.sql
@@ -2,8 +2,5 @@ CREATE TABLE IF NOT EXISTS floor_price_diffs(
    collection_id VARCHAR(64) UNIQUE,
    previous_price TEXT NOT NULL,
    current_price TEXT NOT NULL,
-   last_updated TIMESTAMP NOT NULL DEFAULT NOW(),
-   CONSTRAINT fk_collection_id
-      FOREIGN KEY(collection_id)
-      REFERENCES collections(id)
+   last_updated TIMESTAMP NOT NULL DEFAULT NOW()
 );
